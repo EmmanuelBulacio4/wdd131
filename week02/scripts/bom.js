fc60d98a0a2655718955f3d5382dde4255f92e28
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2025-05-15 08:52:09 -0300

    2namesVariables-reviewed

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,19 +1,19 @@
 // Step 5 CRear variables para input, button y list
 const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
+const addButton = document.querySelector('button');
 const list = document.querySelector('#list');
 
 // Step 6 crear el elemento li
 const entry = document.createElement('li');
 
 // Step 7 creo el boton borrar
-const delbutton = document.createElement('button');
+const delButton = document.createElement('button');
 
 // Step 8 tomo el valor de la entrada y lo asigno al elemento li
 entry.textContent = input.value;
 
 // Step 9 doy imagen al boton borrar
-button.textContent = '❌';
+delButton.textContent = '❌';
 
 // Step 10
 list.append(entry)
