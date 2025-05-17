6dabfedf03c15d397ca36a47e8b9baee277227a2
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-05-17 19:34:44 -0300

    bom.js-ready

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -5,20 +5,22 @@ const list = document.querySelector('#list');
 
 addButton.addEventListener('click', () => {
     const myItem = input.value;
-    input.value = '';
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
+    if (input.value.trim() !==''){
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
 
-    listItem.appendChild(listText);
-    listText.textContent = myItem;
-    listItem.appendChild(listBtn);
-    listBtn.textContent = '❌';
-    list.appendChild(listItem);
-
-    listBtn.addEventListener('click', () => {
-        list.removeChild(listItem);
-    })
-    input.focus();
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = '❌';
+        list.appendChild(listItem);
+        
+        listBtn.addEventListener('click', () => {
+            list.removeChild(listItem);
+        })
+        input.value = '';
+        input.focus();
+    }
 });
