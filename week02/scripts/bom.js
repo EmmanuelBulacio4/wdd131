5a4b00e493cfddcd3f3232efbd846fca06357154
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-05-17 18:41:46 -0300

    entry_changedTo_listItem

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,16 +4,16 @@ const addButton = document.querySelector('button');
 const list = document.querySelector('#list');
 
 // Step 6 crear el elemento li
-const entry = document.createElement('li');
+const listItem = document.createElement('li');
 
 // Step 7 creo el boton borrar
 const delButton = document.createElement('button');
 
 // Step 8 tomo el valor de la entrada y lo asigno al elemento li
-entry.textContent = input.value;
+listItem.textContent = input.value;
 
 // Step 9 doy imagen al boton borrar
 delButton.textContent = '❌';
 
 // Step 10
-list.append(entry)
+list.append(listItem)
