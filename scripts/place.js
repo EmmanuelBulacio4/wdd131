0bed0f6e226d5d025aa3dd9e1854a6312fc89626
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-09-27 16:18:25 -0300

    place-large_on process

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,10 @@ const year = today.getFullYear();
 document.querySelector("#currentyear").textContent = year;
 let lastModification = new Date(document.lastModified)
 document.getElementById("lastModified").textContent = lastModification.toLocaleDateString("en-US");
+
+// Function WindChill
+const wind = document.getElementById(speed);
+
+
+
+// Function WindChill
