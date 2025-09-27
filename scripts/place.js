3f6837cc5b80ddd8cebf13872d500104da4c8c98
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-09-27 18:19:21 -0300

    w3_place.html_ready

--- scripts/place.js
+++ scripts/place.js
@@ -5,20 +5,20 @@ document.querySelector("#currentyear").textContent = year;
 let lastModification = new Date(document.lastModified)
 document.getElementById("lastModified").textContent = lastModification.toLocaleDateString("en-US");
 
-// Function WindChill
-const windSpeed = document.getElementById(speed);
-const temp = document.getElementById(temperature);
+// WindChill - obtener valores
+const windSpeed = parseFloat(document.getElementById(speed));
+const temp = parseFloat(document.getElementById(temperature));
 
-
-
+document.getElementById("windchill").textContent = calculateWindChill(temp, windSpeed)
 
 // Function WindChill
 
 function calculateWindChill (degree, wind) {
-    if (degree>=10 && wind>4.8){
-        let windChill = 13.12 + 0.6215*degree - 11.37*(wind**0.16)+0.3965*degree*(wind**0.16);
-        return windChill
+    if (degree <= 10 && wind > 4.8){
+        let windChill = 13.12 + 0.6215 * degree - 11.37 * (wind ** 0.16) + 0.3965 * degree * (wind ** 0.16);
+        return windChill.toFixed(1)
     }
-    else
+    else{
         return "N/A"
+    }   
 }
