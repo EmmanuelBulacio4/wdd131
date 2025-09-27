b361c7be9bddcde4a5ae1b8469ef88ac1d43f284
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-09-27 17:27:47 -0300

    place.js-functionready

--- scripts/place.js
+++ scripts/place.js
@@ -6,8 +6,19 @@ let lastModification = new Date(document.lastModified)
 document.getElementById("lastModified").textContent = lastModification.toLocaleDateString("en-US");
 
 // Function WindChill
-const wind = document.getElementById(speed);
+const windSpeed = document.getElementById(speed);
+const temp = document.getElementById(temperature);
+
 
 
 
 // Function WindChill
+
+function calculateWindChill (degree, wind) {
+    if (degree>=10 && wind>4.8){
+        let windChill = 13.12 + 0.6215*degree - 11.37*(wind**0.16)+0.3965*degree*(wind**0.16);
+        return windChill
+    }
+    else
+        return "N/A"
+}
