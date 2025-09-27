d9978828d275e3bb20e97a987e8ce3b5b450448d
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-09-26 21:19:24 -0300

    place web in process

---
+++ scripts/place.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+//Año y Fecha en el Footer
+const today = new Date();
+const year = today.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+let lastModification = new Date(document.lastModified)
+document.getElementById("lastModified").textContent = lastModification.toLocaleDateString("en-US");
