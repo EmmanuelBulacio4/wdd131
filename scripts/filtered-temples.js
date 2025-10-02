efdd88b25cf5795c1f273e75ccfd1b7efb93da7e
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-10-02 20:47:50 -0300

    createTempleCard

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -101,3 +101,35 @@ const temples = [
     },
 
 ];
+
+
+
+createTempleCard();
+
+function createTempleCard(){
+    temples.forEach(temple => {
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let location = document.createElement("p");
+        let dedication = document.createElement("p");
+        let area = document.createElement("p");
+        let img = document.createElement("img");
+
+        name.textContent(temple.templeName);
+        location.innerHTML = `<span class = "label">Location:</span> ${temple.location}`;
+        dedication.innerHTML = `<span class = "label">Location:</span> ${temple.dedicated}`;
+        area.innerHTML = `<span class = "label">Location:</span> ${temple.area}`;
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", `${temple.templeName} Temple`);
+        img.setAttribute("loading", "lazy");
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+        
+        documento.getElementById("temples-grid").appendChild(card);
+    }
+    );
+}
