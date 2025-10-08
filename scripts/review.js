be087f3c61c80c7d04c38d846f89b026d2ebe860
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-10-08 17:01:08 -0300

    render_the counter

--- scripts/review.js
+++ scripts/review.js
@@ -2,18 +2,17 @@
 let counter = 0;
 
 window.addEventListener('DOMContentLoaded', function() {
-    const label = document.querySelector("strong");      //Guardo la etiqueta en una variable
-    const rev = localStorage.getItem("reviews-did");    //Tomo el valor de la clave y lo guardo en una variable
+    const label = document.querySelector("strong");
+    const rev = localStorage.getItem("reviews-did"); 
     if ((parseInt(rev) === 0) || (rev === NaN) || (rev === null)){ 
-        counter++;                           //Controlo que el valor de la clave no sea 0                                 //El contador será 1
-        label.textContent = counter;
+        counter++;                          
         localStorage.setItem("reviews-did", counter);
-
+        label.textContent = counter;
     }
     else {
         counter = parseInt(rev);
         counter ++;
         localStorage.setItem("reviews-did", counter);
-        label.textContent = rev;
+        label.textContent = counter;
     }
 });
