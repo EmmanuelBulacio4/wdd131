6132d3a73f251572f3b8060b23a74aa4b1771ad6
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-10-08 16:16:06 -0300

    form-ready

--- scripts/review.js
+++ scripts/review.js
@@ -4,14 +4,15 @@ let counter = 0;
 window.addEventListener('DOMContentLoaded', function() {
     const label = document.querySelector("strong");
     const rev = localStorage.getItem("reviews-did");
-    if (Number(rev) === 0) {
-        const counter = 1;
-        rev.localStorage.setItem("review-did", counter);
+    if (parseInt(rev) === 0) {
+        let counter = 1;
         label.textContent = counter;
+        label.textContent = counter;
+        localStorage.setItem("review-did", counter);
 
     }
     else {
-        counter = Numer(rev);
+        counter = parseInt(rev);
         label.textContent = rev;
         counter ++;
         localStorage.setItem("reviews-did", counter);
