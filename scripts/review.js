1c6479449cb51a236e798413561ed0592c1c8496
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-10-08 16:44:51 -0300

    form-review.js reviewed counter

--- scripts/review.js
+++ scripts/review.js
@@ -4,16 +4,16 @@ let counter = 0;
 window.addEventListener('DOMContentLoaded', function() {
     const label = document.querySelector("strong");      //Guardo la etiqueta en una variable
     const rev = localStorage.getItem("reviews-did");    //Tomo el valor de la clave y lo guardo en una variable
-    if ((parseInt(rev) === 0) || (rev === NaN) || (rev === null)){                            //Controlo que el valor de la clave no sea 0
-        let counter = 1;                                  //El contador será 1
+    if ((parseInt(rev) === 0) || (rev === NaN) || (rev === null)){ 
+        counter++;                           //Controlo que el valor de la clave no sea 0                                 //El contador será 1
         label.textContent = counter;
-        localStorage.setItem("review-did", counter);
+        localStorage.setItem("reviews-did", counter);
 
     }
     else {
         counter = parseInt(rev);
-        label.textContent = rev;
         counter ++;
         localStorage.setItem("reviews-did", counter);
+        label.textContent = rev;
     }
 });
