b7c48d72c4d0776386ed3838415c473786c629c2
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-10-08 16:34:58 -0300

    If condition reviewed

--- scripts/review.js
+++ scripts/review.js
@@ -2,11 +2,10 @@
 let counter = 0;
 
 window.addEventListener('DOMContentLoaded', function() {
-    const label = document.querySelector("strong");
-    const rev = localStorage.getItem("reviews-did");
-    if (parseInt(rev) === 0) {
-        let counter = 1;
-        label.textContent = counter;
+    const label = document.querySelector("strong");      //Guardo la etiqueta en una variable
+    const rev = localStorage.getItem("reviews-did");    //Tomo el valor de la clave y lo guardo en una variable
+    if ((parseInt(rev) === 0) || (rev === NaN) || (rev === null)){                            //Controlo que el valor de la clave no sea 0
+        let counter = 1;                                  //El contador será 1
         label.textContent = counter;
         localStorage.setItem("review-did", counter);
 
